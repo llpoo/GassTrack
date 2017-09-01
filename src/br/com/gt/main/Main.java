95e985ae85b665bbb8a6eed9e0821c53f10aa4d3
lsdourado <lucas.sdourado@outlook.com>
2017-09-01 16:19:55 -0300

    Revert "teste4"
    This reverts commit 5b9f0e3a8b8e393a5233bfd51b5b65628d59b17d.

--- src/br/com/gt/main/Main.java
+++ src/br/com/gt/main/Main.java
@@ -22,7 +22,6 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         LoginController controleLogin = new LoginController();
-        aaaaaaa
     }
     
 }
